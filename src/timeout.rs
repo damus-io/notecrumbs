ab5c82e0e9ca41dad7558c2f5bcdcb161a93a184
William Casarin <jb55@jb55.com>
2025-10-22 09:56:44 -0700

    add missing timeout file
    Signed-off-by: William Casarin <jb55@jb55.com>

---
+++ src/timeout.rs
@@ -0,0 +1,7 @@
+use std::time::Duration;
+
+pub fn get_env_timeout() -> Duration {
+    let timeout_env = std::env::var("TIMEOUT_MS").unwrap_or("2000".to_string());
+    let timeout_ms: u64 = timeout_env.parse().unwrap_or(2000);
+    Duration::from_millis(timeout_ms)
+}

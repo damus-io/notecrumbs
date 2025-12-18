f656b7fc8f8fefe48e3a075f18388cfbb1caaaec
William Casarin <jb55@jb55.com>
2025-12-18 10:07:46 -0800

    nix: pin nixpkgs
    Signed-off-by: William Casarin <jb55@jb55.com>

--- shell.nix
+++ shell.nix
@@ -1,4 +1,5 @@
-{ pkgs ? import <nixpkgs> {} }:
+{ pkgs ? import (fetchTarball "https://github.com/NixOS/nixpkgs/archive/1306659b587dc277866c7b69eb97e5f07864d8c4.tar.gz") {} }:
+
 with pkgs;
 mkShell {
   nativeBuildInputs = [ libiconv pkg-config fontconfig freetype openssl ];
